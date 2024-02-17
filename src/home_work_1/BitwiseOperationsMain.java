b55c8eb26ad480c3caee659a16a8ecbb18bbd473
konstantin-f13 <konstantin.fw13@gmail.com>
2024-02-17 16:20:15 +0300

    Добавлен класс по 1.3, а также комментарии по 1.3 в класс BitwiseOperationsMain

--- src/home_work_1/BitwiseOperationsMain.java
+++ src/home_work_1/BitwiseOperationsMain.java
@@ -14,5 +14,18 @@ public class BitwiseOperationsMain {
         System.out.println("Result of bitwise AND operation: " + c);// bitwise result c = 0001_1000
         int d = a | b;
         System.out.println("Result of bitwise OR operation: " + d);// bitwise result c = 1011_1100
+
+        /*
+        * Комментарий к заданию 1.3 (попытка проведения побитовой операции с литералом):
+        * В задании было указано попробовать провести побитовую операцию с литералом 42.5,
+        * однако мы не можем проводить побитовые операции с числами нецелочисленных типов.
+        * Так как побитовые операторы могут быть применены только к целочисленным типам:
+        * char, byte, int, short, long.
+        * Результат побитовой операции с литералами типов int и float:
+        *
+        *   java: bad operand types for binary operator '&'
+        *   first type:  int
+        *   second type: float
+        *  */
     }
 }
---
+++ src/home_work_1/BitwiseOperationsNonIntegral.java
@@ -0,0 +1,15 @@
+package home_work_1;
+
+public class BitwiseOperationsNonIntegral {
+    public static void main(String[] args) {
+        int a = 56;
+        float b = 42.5;
+
+/*        int c = a & b;
+        System.out.println("Result of bitwise AND operation: " + c);
+        int d = a | b;
+        System.out.println("Result of bitwise OR operation: " + d);*/
+
+
+    }
+}
