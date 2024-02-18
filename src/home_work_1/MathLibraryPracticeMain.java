f74b356bbb2f532958aaf81627bbca186c48259e
konstantin-f13 <konstantin.fw13@gmail.com>
2024-02-18 12:39:54 +0300

    added some cases of usage of the methods of the Math library

--- src/home_work_1/MathLibraryPracticeMain.java
+++ src/home_work_1/MathLibraryPracticeMain.java
@@ -1,4 +1,55 @@
 package home_work_1;
 
 public class MathLibraryPracticeMain {
+    public static void main(String[] args) {
+
+        double a = -505.3324324;
+        double b;
+        b = Math.abs(a);
+        System.out.println("Значение числа a по модулю = " + a);
+        System.out.println("Значение числа b по модулю = " + b);//Returns the absolute value of a double value a.
+        System.out.println("Вычисленное значение синуса угла a = " + Math.sin(Math.toRadians(a)));
+        System.out.println("Вычисленное значение синуса угла b = " + Math.sin(Math.toRadians(b)));/*Вычисление значения
+        синуса угла, указанного в радианах. Math.toRadians - перевод градусов в радианы.*/
+        System.out.println("Вычисление ulp - дистанции между значением и ближайшим большим значением = " + Math.ulp(b));
+
+        double c = 12;
+        double d = 2;
+        System.out.println("Возведение числа c в степень d = " + Math.pow(c, d));
+        System.out.println("Вычисление минимального из двух значений = " + Math.min(c, d));
+        System.out.println("Вычисление минимального из двух значений = " + Math.max(c, d));
+
+        double e = 5.6;
+        System.out.println("Наименьшее целое число, которое >= e = " + Math.ceil(e));
+        System.out.println("Наибольшее целое число, которое <= e = " + Math.floor(e));
+        System.out.println("Округление в большую сторону до ближайшего целого = " + Math.round(e));
+
+
+        System.out.println("Возвращение рандомного числа, >=0 и <1: " + Math.random());
+
+        System.out.println("Вычисление квадратного корня числа 9 = " + Math.sqrt(9)); //3.0
+        System.out.println("Вычисление квадратного корня числа 144 = " + Math.sqrt(144)); //12.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
--- src/home_work_1/README.md
+++ src/home_work_1/README.md
@@ -2,9 +2,9 @@
 ## Описание соответствия классов заданиям
 
 1. Задача 1
-    - 1.1 BitwiseOperationsMain
-    - 1.2 BitwiseOperationsMain
-    - 1.3 BitwiseOperationsMain (комментарий), BitwiseOperationsNonIntegral (класс, в котором была совершена попытка провести побитовую операцию с нецелочисленным числом)
+    - 1.1 класс BitwiseOperationsMain
+    - 1.2 класс BitwiseOperationsMain
+    - 1.3 класс BitwiseOperationsMain (комментарий), BitwiseOperationsNonIntegral (класс, в котором была совершена попытка провести побитовую операцию с нецелочисленным числом)
 2. Задача 2 - класс OperatorsPriorityMain
-3. Пункт третий
+3. Задача 3 - 
 4. ...
