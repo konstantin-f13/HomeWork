d270a6d1a909e549829da54ee2ae3c3b5b2b59a7
konstantin-f13 <konstantin.fw13@gmail.com>
2024-02-19 16:24:20 +0300

    Task 6.2 solved

--- src/home_work_1/NameInquiryIfElseIf.java
+++ src/home_work_1/NameInquiryIfElseIf.java
@@ -1,4 +1,21 @@
 package home_work_1;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class NameInquiryIfElseIf {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите Ваше имя");
+        String name = console.next();
+        String str1 = "Вася";
+        String str2 = "Анастасия";
+        if (Objects.equals(name, str1)) {
+            System.out.println("Привет!" + "\n" + "Я тебя так долго ждал");
+        } else if (Objects.equals(name, str2)) {
+            System.out.println("Я тебя так долго ждал");
+        } else {
+            System.out.println("Добрый день! А вы кто?");
+        }
+    }
 }
