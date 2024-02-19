d97cc2c79347c0fc2d399055e7b834c844c48075
konstantin-f13 <konstantin.fw13@gmail.com>
2024-02-19 16:56:21 +0300

    Task 6.3 solved

--- src/home_work_1/NameInquirySwitch.java
+++ src/home_work_1/NameInquirySwitch.java
@@ -1,4 +1,21 @@
 package home_work_1;
 
+import java.util.Scanner;
+
 public class NameInquirySwitch {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите Ваше имя");
+        String name = console.next();
+        switch (name) {
+            case "Вася":
+                System.out.println("Привет!" + "\n" + "Я тебя так долго ждал");
+                break;
+            case "Анастасия":
+                System.out.println("Я тебя так долго ждал");
+                break;
+            default:
+                System.out.println("Добрый день! А вы кто?");
+        }
+    }
 }
