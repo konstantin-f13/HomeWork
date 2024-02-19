8f497aec99bb3e4826e7c872d712cb5bd0fc8979
konstantin-f13 <konstantin.fw13@gmail.com>
2024-02-19 22:35:28 +0300

    Task 7 added (empty, not solved)

---
+++ src/home_work_1/CreatePhoneNumberMain.java
@@ -0,0 +1,12 @@
+package home_work_1;
+
+public class CreatePhoneNumberMain {
+    public static void main(String[] args) {
+
+
+    }
+
+    static String createPhoneNumber(int[] phoneNumber){
+
+    }
+}
