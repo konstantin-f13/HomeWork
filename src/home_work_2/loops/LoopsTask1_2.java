8cc766e61cd03335f3121806677736ea82801b98
konstantin-f13 <konstantin.fw13@gmail.com>
2024-02-24 15:27:42 +0300

    Task 1.2 solved and readme updated

---
+++ src/home_work_2/loops/LoopsTask1_2.java
@@ -0,0 +1,34 @@
+package home_work_2.loops;
+
+/*    1.2. Все цифры из числа введенного через аргумент к исполняемой программе перемножить между собой и вывести
+ход вычислений в консоль. Пользователь обязан ввести целое число. Если ввели не целое то выдать сообщение о том что
+пользователь ввёл некорректные данные.*/
+
+public class LoopsTask1_2 {
+    public static void main(String[] args) {
+
+        String strNum = args[0];
+        boolean onlyDigits = strNum.matches("\\d+");
+        boolean nonIntNumber = strNum.matches("\\d++[.]\\d++");
+        boolean notNumber = strNum.matches("\\D++");
+        long digitsMultiplication = 1;
+
+        if (onlyDigits) {
+            System.out.println("Заданное через аргумент к программе целое число = " + strNum);
+            System.out.print("Произведение всех цифр заданного числа = ");
+
+            for (int i = 0; i < strNum.length(); i++) {
+                System.out.print(strNum.charAt(i) + " * ");
+                digitsMultiplication = digitsMultiplication * Character.getNumericValue(strNum.charAt(i));
+            }
+
+            System.out.println("\b\b" + "= " + digitsMultiplication);
+        } else if (nonIntNumber) {
+            System.out.println("Введено нецелое число.");
+        } else if (notNumber) {
+            System.out.println("Введено не число, а вообще что-то другое");
+        } else {
+            System.out.println("Другая ошибка");
+        }
+    }
+}
