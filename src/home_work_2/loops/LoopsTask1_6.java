fbad986642d2be6a1ec2aee69db4541f99e7523d
konstantin-f13 <konstantin.fw13@gmail.com>
2024-02-25 14:17:07 +0300

    Task 1.6 updated

--- src/home_work_2/loops/LoopsTask1_6.java
+++ src/home_work_2/loops/LoopsTask1_6.java
@@ -7,14 +7,22 @@ public class LoopsTask1_6 {
             for (int j = 2; j <= 5 ; j++) {
                 System.out.print(" " + j + " x " + i + " = " + i * j + " |");
             }
-            System.out.println("");
+            System.out.println("\b\b");
         }
+
+        int count = 0;
+
+        while (count <= 51) {
+            System.out.print("_");
+            count++;
+        }
+        System.out.println("");
 
         for (int i = 1; i <= 10; i++) {
             for (int j = 6; j <= 9 ; j++) {
                 System.out.print(" " + j + " x " + i + " = " + i * j + " |");
             }
-            System.out.println("");
+            System.out.println("\b\b");
         }
     }
 }
