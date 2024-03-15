4acb694572e8e48dc826317d78953aff4b9d47ea
konstantin-f13 <konstantin.fw13@gmail.com>
2024-03-15 18:08:32 +0300

    Folder home_work_3 created. Task 1.1 done

---
+++ src/home_work_3/runners/WithoutCalculatorMain.java
@@ -0,0 +1,8 @@
+package home_work_3.runners;
+
+public class WithoutCalculatorMain {
+    public static void main(String[] args) {
+        System.out.printf("%.2f", 4.1f + 15 * 7 + ((28f / 5) * (28f / 5)));//=140,46
+    }
+
+}
